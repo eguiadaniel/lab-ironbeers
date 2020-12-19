d465296b0869583b1aa4c64e6544ecd46f545891
eguiadaniel <eguiadaniel@users.noreply.github.com>
2020-12-19 11:19:50 +0100

    links  and routes working

--- app.js
+++ app.js
@@ -14,12 +14,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(__dirname + '/views/partials');
+app.use(express.static('public'));
 
 // Add the route handlers here:
 
+app.get('/:route', (request, response) => {
+  const name = request.params.route;
+  switch (name) {
+    case 'home':
+      response.render('index', { message: 'home' });
+      break;
+    case 'beers':
+      response.render('index', { message: 'beers' });
+      break;
+    case 'random-beer':
+      response.render('index', { message: 'random' });
+      break;
+    default:
+      response.render('index', { message: `default ${name}` });
+  }
+});
+
+/*
 app.get('/', (req, res) => {
   res.render('index');
 });
+*/
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
